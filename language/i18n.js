524af2e9e0eb1c987d4b980268ed50783d309a78
bcachain <bcachain@gmail.com>
2018-09-25 08:44:44 +0800

    add thai

--- language/i18n.js
+++ language/i18n.js
@@ -1,7 +1,7 @@
 import I18n, { getLanguages } from 'react-native-i18n';
 import en from './en'; //英文
 import zh from './zh'; //中文
-// import th from './th'; //泰国
+import th from './th'; //泰国
 // import ja from './ja'; //日本
 // import vi from './vi'; //越南语
 
@@ -12,7 +12,7 @@ I18n.fallbacks = true;
 I18n.translations = {
 	zh,
 	en,
-	// th,
+	th,
 	// ja,
 	// vi
 };
@@ -20,7 +20,7 @@ I18n.translations = {
 const languagePackAll_Data = {
 	zh: I18n.t('my.sysSetting.language.changeToChinese'),
 	en: I18n.t('my.sysSetting.language.changeToEnglish'),
-	// th: I18n.t('my.sysSetting.language.changeToThai'),
+	th: I18n.t('my.sysSetting.language.changeToThai'),
 	// ja: I18n.t('my.sysSetting.language.changeToJp'),
 	// vi: I18n.t('my.sysSetting.language.changeToVi'),
 
